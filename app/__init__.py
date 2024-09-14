d8dd8055716c7034da5a80583562ef26e6302762
Abbas <abbasmurudkar118@icloud.com>
2024-09-14 18:52:48 +0530

    Inital Commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,25 +3,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
+    load_dotenv()  
     app = Flask(__name__)
     CORS(app)
 
-    # Load configuration from object
+
     app.config.from_object('app.config.Config')
 
-    # Set database URL from environment variable
+  
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
-    # Initialize extensions
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Import and register routes
+    
     with app.app_context():
         from . import routes
         db.create_all()
